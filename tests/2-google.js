f8e83ddd09d8075623568cdae5033c338c688a01
Dennis Whalen <dwwhalen@gmail.com>
2019-01-15 18:42:51 -0500

    updates

--- tests/2-google.js
+++ tests/2-google.js
@@ -1,12 +1,22 @@
 const puppeteer = require('puppeteer');
+const { expect } = require('chai');
+
+// puppeteer options
+const opts = {
+  headless: false,
+  slowMo: 100,
+  timeout: 10000
+};
 
 (async () => {
-  const browser = await puppeteer.launch({headless:false});
+  const browser = await puppeteer.launch(opts);
   const page = await browser.newPage();
   await page.setViewport({ width: 1680, height: 1050 })
   await page.goto('https://www.google.com', {waitUntil: 'networkidle2'});
   await console.log('search page loaded');
-  const searchTextbox = await page.$('input[type=text]');
+
+  const searchTextbox = await page.waitFor('input[name=q]');
+  
   await searchTextbox.type('meeseek');
   await page.keyboard.press('Enter');
 
@@ -14,6 +24,8 @@ const puppeteer = require('puppeteer');
     page.waitForNavigation(),
     page.once('load', () => console.log('meeseek results page loaded'))
   ]);
+
+  expect(await page.title()).to.contain('Google Search');
 
   await page.screenshot({path: 'meeseek.png'});
 
